248e56461269a1bb781e58d4a5cf0f335a6d0a11
temuv8-design <temuv8@gmail.com>
2026-01-27 20:04:54 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "name": "bot-inventario-repuestos",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "telegraf": "^4.16.2",
+    "axios": "^1.6.7",
+    "google-spreadsheet": "^4.1.2"
+  }
+}
